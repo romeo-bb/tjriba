32f57f0822d76e596525ddcbe7a4b613bb230310
p <p@MacBook-Air-de-p.local>
2023-10-21 13:03:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import bg_vid from "./Vhome.mp4";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='h-[100vh] w-full flex justify-center items-center bg-slate-300'>
+        <div className='w-[250px] h-[250px]'>
+        <video
+          src={bg_vid}
+          className='w-full h-full object-cover'
+          autoPlay
+          loop
+          muted>
+        </video>
+        </div>
+      </div>
     </div>
   );
 }
